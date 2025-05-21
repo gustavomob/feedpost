8f8660af6dbe35e1cca56fdd698ba9b2427b1d85
gustavomob <gustavogss.mob@gmail.com>
2025-05-20 22:05:12 -0300

    feat: setup project

---
+++ src/screens/home/index.js
@@ -0,0 +1,8 @@
+import { Container, Title } from "./styles"
+
+export function Home() {
+ return (
+<Container>
+<Title>FeedPost</Title>
+</Container>
+)};
---
+++ src/screens/home/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+align-items: center;
+justify-content: center;
+background-color: #EEE;
+`;
+ 
+export const Title = styled.Text`
+font-size:20px;
+font-weight: 400;
+`;
